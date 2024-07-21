a6b4c4b28d4077cc5933ee8b8cc36fc22cce4cda
Ahmad Jamal <ahmadjamal6886@gmail.com>
2024-07-21 21:29:10 +0500

    update

--- 11-July/11-july-class01.py
+++ 11-July/11-july-class01.py
@@ -1,6 +1,2 @@
-print("Hello Ahmad Jamal Here now we working on python 1st class.")
-print("Hello Ahmad Jamal Here now we working on python 1st class.")
-
-print("Hello Ahmad Jamal Here now we working on python 1st class.")
-print("Hello Ahmad Jamal Here now we working on python 1st class.")
-print("Hello Ahmad Jamal Here now we working on python 1st class.")
+name: str = 5
+print(name)
