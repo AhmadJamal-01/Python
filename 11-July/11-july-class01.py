e496594dd6a455f27ddef5aa45476c996a5c5d2a
Ahmad Jamal <ahmadjamal6886@gmail.com>
2024-08-10 15:50:19 +0500

    Assignments

--- 11-July/11-july-class01.py
+++ 11-July/11-july-class01.py
@@ -2,3 +2,31 @@
 #mypy filename.py (for error checking)
 name: str = 'Helo Ahmad Jamal'
 print(name)
+
+#if you want use ' in chracter then you use around "
+message:str = "PIAIC Student Card \nFather's Name"
+print(message)
+
+#convert any special chracter in simple chracter, place\ before chracter
+messages:str='Piaic Student Card\n father\'s Name'
+
+names:str = "Ahmad Jamal"
+fname:str = "Afzaal Hussain"
+education:str ="Master in Data Science"
+age:int = 30
+
+card:str ="Piaic Student Card\n Student Name:" + names
+print(card) 
+
+#Define Multiline String """ """ """"
+
+# cards:str = """
+# Piaic Student Card
+# Student Name : {name}
+# Father's Name: {fname}
+# Age: {age}
+# Education : {education}
+# """
+# print (cards)
+
+
---
+++ Piaic-Python-Assignment/assignment-01-29july/Readme.md
@@ -0,0 +1,17 @@
+<!-- What are GPTs? What are features and use cases of custom GPTs? -->
+
+Features
+
+Personalization -GPTs can be adjusted to follow specific styles, tones, or behaviors, making interactions more aligned with individual or business preferences.
+Task Optimization
+Integration with APIs and Tools
+Enhanced Privacy
+
+
+Use Cases
+
+Customer Support
+Content Creation
+Education and Training
+Healthcare
+Software Development
