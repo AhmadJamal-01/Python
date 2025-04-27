13d30f87f46e4876f56d16425ec4e15841bfa6d0
Ahmad Jamal <150580198+AhmadJamal-01@users.noreply.github.com>
2025-04-27 18:29:33 +0000

    Basic Calculator

---
+++ python_projects/basic_calculator.py
@@ -0,0 +1,12 @@
+#Python Basic Calculator
+
+operator = input("Enter an Operator(+ - * /):")
+first_number = float(input("Enter First Number : "))
+second_number = float(input("Enter Second Number : "))
+
+if operator == "+" : (
+   print("Total Sum is:", first_number + second_number)
+)
+else: (
+    print("nothing")
+)
