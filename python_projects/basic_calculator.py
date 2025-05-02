7f8b9cf9ed85b7e99987e1570edeeb6d431f38a0
Ahmad Jamal <150580198+AhmadJamal-01@users.noreply.github.com>
2025-05-02 18:37:46 +0000

    Basic Calculator

--- python_projects/basic_calculator.py
+++ python_projects/basic_calculator.py
@@ -1,12 +1,31 @@
 #Python Basic Calculator
 
+print("Welcome to the Basic Calculator")
+print("You can perform the following operations:")
+
 operator = input("Enter an Operator(+ - * /):")
 first_number = float(input("Enter First Number : "))
 second_number = float(input("Enter Second Number : "))
 
-if operator == "+" : (
+if operator == "+" : 
    print("Total Sum is:", first_number + second_number)
-)
-else: (
-    print("nothing")
-)
+
+
+elif operator == "-" : 
+   print("Total Subtraction is:", first_number - second_number)
+
+elif operator == "*" : 
+    print("Total Multiplication is:", first_number * second_number)
+    
+elif operator == "/" : 
+    if second_number == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        print("Total Division is:", first_number / second_number)
+
+elif operator == "%" : 
+    print("Total Modulus is:", first_number % second_number)
+
+else: 
+    print(f"Invalid operator: {operator}. Please use +, -, *, /, or %.")
+#End of the code
